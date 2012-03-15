fadad152162e00afa77abb95366e76510abd1e20
Thrasibule <guillaume.horel@gmail.com>
2012-03-15 18:26:26 -0400

    [dev-ml/typerex] version bump

---
+++ dev-ml/typerex/files/50typerex-gentoo.el
@@ -0,0 +1,10 @@
+;;Loading TypeRex mode for OCaml files
+(add-to-list 'load-path "@SITELISP@")
+(add-to-list 'auto-mode-alist '("\\.ml[iylp]?" . typerex-mode))
+(add-to-list 'interpreter-mode-alist '("ocamlrun" . typerex-mode))
+(add-to-list 'interpreter-mode-alist '("ocaml" . typerex-mode))
+(autoload 'typerex-mode "typerex" "Major mode for editing Caml code" t)
+
+;; TypeRex mode configuration
+(setq ocp-server-command "/usr/bin/ocp-wizard")
+(setq-default indent-tabs-mode nil)
